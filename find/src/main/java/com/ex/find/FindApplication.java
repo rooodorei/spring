29d4665dea13eb787763d78041677c85d662df5e
rooodorei <2155536845@qq.com>
2025-02-19 16:01:35 +0800

    1

---
+++ find/src/main/java/com/ex/find/FindApplication.java
@@ -0,0 +1,13 @@
+package com.ex.find;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindApplication.class, args);
+	}
+
+}
---
+++ find/src/test/java/com/ex/find/FindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.find;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
